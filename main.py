4437bb5539bcad92ff05f69364e3e8b6a1297a22
Yahya Najar <mynajar04@gmail.com>
2023-09-23 12:27:23 +0530

    working game

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Tracking Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_A = turtle.Turtle()
 paddle_A.speed(0)
@@ -27,12 +31,21 @@ paddle_B.goto(350, 0)
 # Ball
 Ball = turtle.Turtle()
 Ball.speed(0)
-Ball.shape("square")
+Ball.shape("circle")
 Ball.color("red")
 Ball.penup()
 Ball.goto(0, 0)
-Ball.dx = 0.06
-Ball.dy = 0.06
+Ball.dx = 0.1
+Ball.dy = 0.1
+
+# Making the Scoreboard
+Score_Card = turtle.Turtle()
+Score_Card.speed(0)
+Score_Card.color("white")
+Score_Card.penup()
+Score_Card.hideturtle()
+Score_Card.goto(0, 260)
+Score_Card.write("Player A: 0  Player B: 0", align="center", font=("Times New Roman", 24, "normal"))
 
 
 # Function
@@ -81,13 +94,32 @@ while True:
         Ball.dy *= -1
 
     if Ball.ycor() < -290:
-        Ball.sety(-290)
+        Ball.sety(-280)
         Ball.dy *= -1
 
     if Ball.xcor() > 390:
         Ball.goto(0, 0)
         Ball.dx = -0.06
+        score_a += 1
+        Score_Card.clear()
+        Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                         font=("Times New Roman", 24, "normal"))
 
     if Ball.xcor() < -390:
         Ball.goto(0, 0)
         Ball.dx = 0.06
+        score_b += 1
+        Score_Card.clear()
+        Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                         font=("Times New Roman", 24, "normal"))
+
+    # Paddle and Ball collision
+    if (Ball.xcor() > 340 and Ball.xcor() < 350) and (
+            Ball.ycor() < paddle_B.ycor() + 40 and Ball.ycor() > paddle_B.ycor() - 40):
+        Ball.setx(340)
+        Ball.dx *= -1.5
+
+    if (Ball.xcor() < -340 and Ball.xcor() > -350) and (
+            Ball.ycor() < paddle_A.ycor() + 40 and Ball.ycor() > paddle_A.ycor() - 40):
+        Ball.setx(-340)
+        Ball.dx *= -1.5
