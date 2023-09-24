85d6dc0b39ee2460d589a196cc39f7e5cd059eff
Yahya Najar <mynajar04@gmail.com>
2023-09-24 19:02:19 +0530

    final game

--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 import turtle
+import winsound
 from items import Paddle, Ball
 from ScoreCard import Score
+import time
 
 wn = turtle.Screen()
 wn.title("Pong Game by Yahya Najar")
@@ -20,7 +22,22 @@ score_a = 0
 score_b = 0
 
 # Making the Scoreboard
-Score_Card = Score("white","Player A: 0  Player B: 0", "center", font="Times New Roman", size=24, fonttype="normal")
+Score_Card = Score("white", "Player A: 0  Player B: 0", "center", "Times New Roman", 24, "normal")
+
+# Making Winning Screen
+Win_A = turtle.Turtle()
+Win_A.speed(0)
+Win_A.color("green")
+Win_A.penup()
+Win_A.hideturtle()
+Win_A.goto(0, 0)
+
+Win_B = turtle.Turtle()
+Win_B.speed(0)
+Win_B.color("green")
+Win_B.penup()
+Win_B.hideturtle()
+Win_B.goto(0, 0)
 
 
 # Function
@@ -48,12 +65,30 @@ def paddle_b_down():
     paddle_B.sety(y)
 
 
+# Restarting the game
+def restart_game():
+    global score_a, score_b
+    Ball.goto(0, 0)
+    Ball.dx = 0.1
+    Ball.dy = 0.1
+    score_a = 0
+    score_b = 0
+    Score_Card.clear()
+    Score_Card.write("Player A: 0  Player B: 0", align="center",
+                     font=("Times New Roman", 24, "normal"))
+    Win_A.clear()
+    Win_B.clear()
+    Ball.showturtle()
+    wn.update()
+
+
 # Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
+wn.onkeypress(restart_game, "r")
 
 # Main Game Loop
 while True:
@@ -88,13 +123,39 @@ while True:
         Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
                          font=("Times New Roman", 24, "normal"))
 
+    # Paddle Border
+    if paddle_A.ycor() > 248:
+        paddle_A.sety(248)
+
+    if paddle_A.ycor() < -240:
+        paddle_A.sety(-240)
+
+    if paddle_B.ycor() > 248:
+        paddle_B.sety(248)
+
+    if paddle_B.ycor() < -240:
+        paddle_B.sety(-240)
+
     # Paddle and Ball collision
-    if (Ball.xcor() > 340 and Ball.xcor() < 350) and (
-            Ball.ycor() < paddle_B.ycor() + 40 and Ball.ycor() > paddle_B.ycor() - 40):
+    if (340 < Ball.xcor() < 350) and (
+            paddle_B.ycor() + 40 > Ball.ycor() > paddle_B.ycor() - 40):
         Ball.setx(340)
         Ball.dx *= -1.15
 
-    if (Ball.xcor() < -340 and Ball.xcor() > -350) and (
-            Ball.ycor() < paddle_A.ycor() + 40 and Ball.ycor() > paddle_A.ycor() - 40):
+    if (-340 > Ball.xcor() > -350) and (
+            paddle_A.ycor() + 40 > Ball.ycor() > paddle_A.ycor() - 40):
         Ball.setx(-340)
         Ball.dx *= -1.15
+
+    if score_a == 5:
+        Ball.dx = 0
+        Ball.dy = 0
+        Win_A.write("Player A wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
+        Ball.hideturtle()
+        # winsound.PlaySound("Win.wav", winsound.SND_ASYNC)
+
+    if score_b == 5:
+        Ball.dx = 0
+        Ball.dy = 0
+        Win_B.write("Player B wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
+        Ball.hideturtle()
