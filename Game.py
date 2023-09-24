50a3228495c172c1321b432c5f6177535ae8ac97
Yahya Najar <mynajar04@gmail.com>
2023-09-24 15:53:29 +0530

    trying to start game

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import turtle
 import winsound
 from items import Paddle, Ball
 from ScoreCard import Score
+import time
 
 wn = turtle.Screen()
 wn.title("Pong Game by Yahya Najar")
@@ -79,9 +80,104 @@ def restart_game():
     Win_A.clear()
     Win_B.clear()
     Ball.showturtle()
+    wn.update()
 
 
-# Starting the game
+# Main Game Loop
+def update_game():
+    global score_a, score_b
+    while True:
+        # Move the Ball
+        Ball.setx(Ball.xcor() + Ball.dx)
+        Ball.sety(Ball.ycor() + Ball.dy)
+
+        # Border Checking
+        if Ball.ycor() > 290:
+            Ball.sety(290)
+            Ball.dy *= -1
+            winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+        if Ball.ycor() < -290:
+            Ball.sety(-280)
+            Ball.dy *= -1
+            winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+        if Ball.xcor() > 390:
+            Ball.goto(0, 0)
+            Ball.dx = -0.06
+            score_a += 1
+            Score_Card.clear()
+            Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                             font=("Times New Roman", 24, "normal"))
+
+        if Ball.xcor() < -390:
+            Ball.goto(0, 0)
+            Ball.dx = 0.06
+            score_b += 1
+            Score_Card.clear()
+            Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                             font=("Times New Roman", 24, "normal"))
+
+        # Paddle Border
+        if paddle_A.ycor() > 248:
+            paddle_A.sety(248)
+
+        if paddle_A.ycor() < -240:
+            paddle_A.sety(-240)
+
+        if paddle_B.ycor() > 248:
+            paddle_B.sety(248)
+
+        if paddle_B.ycor() < -240:
+            paddle_B.sety(-240)
+
+        # Paddle and Ball collision
+        if (340 < Ball.xcor() < 350) and (
+                paddle_B.ycor() + 40 > Ball.ycor() > paddle_B.ycor() - 40):
+            Ball.setx(340)
+            Ball.dx *= -1.175
+            winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+        if (-340 > Ball.xcor() > -350) and (
+                paddle_A.ycor() + 40 > Ball.ycor() > paddle_A.ycor() - 40):
+            Ball.setx(-340)
+            Ball.dx *= -1.175
+            winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+        if score_a == 5:
+            Ball.dx = 0
+            Ball.dy = 0
+            Win_A.write("Player A wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
+            Ball.hideturtle()
+            # winsound.PlaySound("Win.wav", winsound.SND_ASYNC)
+
+        if score_b == 5:
+            Ball.dx = 0
+            Ball.dy = 0
+            Win_B.write("Player B wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
+            Ball.hideturtle()
+            # winsound.PlaySound("Win.wav", winsound.SND_ASYNC)
+            wn.update()
+            wn.ontimer(update_game, 10)
+
+
+def start_game():
+    wn.update()
+
+    global score_a, score_b
+    Ball.goto(0, 0)
+    Ball.dx = 0.1
+    Ball.dy = 0.1
+    score_a = 0
+    score_b = 0
+    Score_Card.clear()
+    Score_Card.write("Player A: 0  Player B: 0", align="center",
+                     font=("Times New Roman", 24, "normal"))
+    Win_A.clear()
+    Win_B.clear()
+    Ball.showturtle()
+    update_game()
+
 
 # Keyboard Binding
 wn.listen()
@@ -90,80 +186,16 @@ wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 wn.onkeypress(restart_game, "r")
-# wn.onkeypress(start_game, "e")
+wn.onkeypress(start_game, "e")
 
+# Starting the game
+start_game()
 
-# Main Game Loop
-while True:
-    wn.update()
+wn.ontimer(update_game, 10)
 
-    # Move the Ball
-    Ball.setx(Ball.xcor() + Ball.dx)
-    Ball.sety(Ball.ycor() + Ball.dy)
+# Update Game
+update_game()
 
-    # Border Checking
-    if Ball.ycor() > 290:
-        Ball.sety(290)
-        Ball.dy *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+wn.update()
 
-    if Ball.ycor() < -290:
-        Ball.sety(-280)
-        Ball.dy *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
-
-    if Ball.xcor() > 390:
-        Ball.goto(0, 0)
-        Ball.dx = -0.06
-        score_a += 1
-        Score_Card.clear()
-        Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
-                         font=("Times New Roman", 24, "normal"))
-
-    if Ball.xcor() < -390:
-        Ball.goto(0, 0)
-        Ball.dx = 0.06
-        score_b += 1
-        Score_Card.clear()
-        Score_Card.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
-                         font=("Times New Roman", 24, "normal"))
-
-    # Paddle Border
-    if paddle_A.ycor() > 248:
-        paddle_A.sety(248)
-
-    if paddle_A.ycor() < -240:
-        paddle_A.sety(-240)
-
-    if paddle_B.ycor() > 248:
-        paddle_B.sety(248)
-
-    if paddle_B.ycor() < -240:
-        paddle_B.sety(-240)
-
-    # Paddle and Ball collision
-    if (340 < Ball.xcor() < 350) and (
-            paddle_B.ycor() + 40 > Ball.ycor() > paddle_B.ycor() - 40):
-        Ball.setx(340)
-        Ball.dx *= -1.175
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
-
-    if (-340 > Ball.xcor() > -350) and (
-            paddle_A.ycor() + 40 > Ball.ycor() > paddle_A.ycor() - 40):
-        Ball.setx(-340)
-        Ball.dx *= -1.175
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
-
-    if score_a == 5:
-        Ball.dx = 0
-        Ball.dy = 0
-        Win_A.write("Player A wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
-        Ball.hideturtle()
-        winsound.PlaySound("Win.mp3", winsound.SND_ASYNC)
-
-    if score_b == 5:
-        Ball.dx = 0
-        Ball.dy = 0
-        Win_A.write("Player B wins \nr to restart", align="center", font=("Times New Roman", 24, "normal"))
-        Ball.hideturtle()
-        winsound.PlaySound("Win.mp3", winsound.SND_ASYNC)
+wn.bye()
